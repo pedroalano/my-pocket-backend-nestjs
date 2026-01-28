d3a3af0651ee139f69a3486acf99a2fc4f18a8d8
Pedro Alano <pedroalano@gmail.com>
2026-01-28 20:01:31 -0300

    feat: add CategoriesModule and register it in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
