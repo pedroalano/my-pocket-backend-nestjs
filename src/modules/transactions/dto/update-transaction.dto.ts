7ad72d7ac35dd7064b84d1929ffee4c4d728cace
Pedro Alano <pedroalano@gmail.com>
2026-02-02 17:24:11 -0300

    Add validation decorators to transaction DTOs
    - Add @IsString() decorators to CreateTransactionDto fields
    - Add @IsString() decorators to UpdateTransactionDto fields
    - Add description field to both DTOs
    - Import IsString validator from class-validator

--- src/modules/transactions/dto/update-transaction.dto.ts
+++ src/modules/transactions/dto/update-transaction.dto.ts
@@ -1,6 +1,14 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class UpdateTransactionDto {
+  @IsNumber()
   amount?: number;
+  @IsString()
   type?: string;
+  @IsNumber()
   categoryId?: number;
+  @IsString()
   date?: string;
+  @IsString()
+  description?: string;
 }
